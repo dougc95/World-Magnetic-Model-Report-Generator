23da05902f742cdb2137c112b4a3f8de4b20604c
Douglas Rojas <dougcr95@gmail.com>
2024-07-06 15:19:54 -0400

    add new flags

--- build.py
+++ build.py
@@ -1,6 +1,10 @@
+import os
 import PyInstaller.__main__
 
 def create_executable():
+    import site
+    site_packages = site.getsitepackages()[0]
+
     PyInstaller.__main__.run([
         'main.py',
         '--name=WMM_Report_Generator',
@@ -19,6 +23,8 @@ def create_executable():
         '--hidden-import=openpyxl',
         '--hidden-import=numpy',
         '--hidden-import=pydantic',
+        f'--add-data={os.path.join(site_packages, "PyQt6", "Qt6", "plugins", "platforms", "*.dll")};PyQt6/Qt6/plugins/platforms',
+        f'--add-data={os.path.join(site_packages, "PyQt6", "Qt6", "bin", "*.dll")};PyQt6/Qt6/bin',
     ])
 
 if __name__ == "__main__":
