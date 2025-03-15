6662fe1f8806bedc4c0c7b37240f1e25c6e68c3c
dougc95 <dougcr95@gmail.com>
2025-03-15 19:17:54 -0400

    fix git issue

--- src/services/wmm_calculator.py
+++ src/services/wmm_calculator.py
@@ -6,44 +6,15 @@ from datetime import datetime, timedelta
 class WMMCalculator:
     @staticmethod
     def calculate(model: WMMModel):
-<<<<<<< Updated upstream
-        decimal_year = date_utils.decimal_year(model.date)
-=======
-<<<<<<< Updated upstream
-        decimal_year = DateUtils.decimal_year(model.date)
->>>>>>> Stashed changes
-        dec = WMMv2.get_declination(
-            model.latitude, model.longitude, decimal_year, model.altitude
-        )
-        dip = WMMv2.get_dip_angle(
-            model.latitude, model.longitude, decimal_year, model.altitude
-        )
-        ti = WMMv2.get_intensity(
-            model.latitude, model.longitude, decimal_year, model.altitude
-        )
-        bh = WMMv2.get_horizontal_intensity(
-            model.latitude, model.longitude, decimal_year, model.altitude
-        )
-        bx = WMMv2.get_north_intensity(
-            model.latitude, model.longitude, decimal_year, model.altitude
-        )
-        by = WMMv2.get_east_intensity(
-            model.latitude, model.longitude, decimal_year, model.altitude
-        )
-        bz = WMMv2.get_vertical_intensity(
-            model.latitude, model.longitude, decimal_year, model.altitude
-        )
-=======
         decimal_year = date_utils.decimal_year(model.date)
         wmm = WMMv2()
         dec = wmm.get_declination(model.latitude, model.longitude, decimal_year, model.altitude)
         dip = wmm.get_dip_angle(model.latitude, model.longitude, decimal_year, model.altitude)
         ti = wmm.get_intensity(model.latitude, model.longitude, decimal_year, model.altitude)
-        bh = wmm.get_horizontal_intensity(model.atitude, model.longitude, decimal_year, model.altitude)
+        bh = wmm.get_horizontal_intensity(model.latitude, model.longitude, decimal_year, model.altitude)
         bx = wmm.get_north_intensity(model.latitude, model.longitude, decimal_year, model.altitude)
         by = wmm.get_east_intensity(model.latitude, model.longitude, decimal_year, model.altitude)
         bz = wmm.get_vertical_intensity(model.latitude, model.longitude, decimal_year, model.altitude)
->>>>>>> Stashed changes
         model.set_results(dec, dip, ti, bh, bx, by, bz)
 
     @staticmethod
